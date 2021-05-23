5e8c2e1d30e5311d3e13c4ff939745e8f18b4c33
Sanket Pawar <pawarsanket668@gmail.com>
2021-05-23 16:25:09 +0530

    third commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import profilepic from '../images/mypicture.jpeg'
+import profilepic from '../images/movies-img.jpg'
 
 const Home =() => {
     return (
-        <div className="row">
-            <div className="col-md-4 col-xs-12"></div>
-            <div className="col-md-4 col-xs-12 text-center">
-             <img src={profilepic} className="img img-thumbnail" alt="Sanket Pawar"/>
+        <div className="row" >
+            <div className="col-md-4 col-xs-12 m-1" ></div>
+            <div className="col-md-4 col-xs-12 m-1 text-center">
+             <img src={profilepic} className="img img-thumbnail movieimg"  alt="Sanket Pawar"/>
                 <h2>Movie Browser App</h2>
                 <p>Powered By ReactJS</p>
                 
